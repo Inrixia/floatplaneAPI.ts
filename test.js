caf58d9d792635fca7597d7e5916d7d815e411d2
Inrix <Inrixia@gmail.com>
2020-09-06 14:10:48 +1200

    Updated test.js to persist session between tests

--- test.js
+++ test.js
@@ -1,20 +1,26 @@
 const fs = require('fs')
 const { lObj } = require('@inrixia/helpers/object')
+const db = require('@inrixia/db')
+
+const credentials = new db('credentials', './credentials.json', false)
 
 const Floatplane = require('./')
 const fapi = new Floatplane()
 
 ;(async () => {
-	const login = await fapi.login(require('./testCredentials.json'))
-	lObj(login)
+	if (!credentials.cookie) { // Session persistance
+		await fapi.login(credentials)
+		credentials.cookie = fapi.auth.cookie
+	} else fapi.auth.cookie = credentials.cookie
+
 	const subs = await fapi.user.subscriptions()
 	lObj(subs)
-	const videos = fapi.creator.videos(subs[0].creator)
-	lObj(await videos.next(), '\n\n\n\n')
-	for await (const video of videos) {
-		console.log(video)
-		break;
-	}
+	// const videos = fapi.creator.videos(subs[0].creator)
+	// lObj(await videos.next(), '\n\n\n\n')
+	// for await (const video of videos) {
+	// 	console.log(video)
+	// 	break;
+	// }
 	
 	// const videoURL = await fapi.video.downloadURL(video.value.guid)
 	// lObj(videoURL)
