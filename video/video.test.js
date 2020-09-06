4e46d4ef39fe2c62e6fd516bd63e99ad798747d0
Inrix <Inrixia@gmail.com>
2020-09-06 16:33:43 +1200

    Added video tests

---
+++ video/video.test.js
@@ -0,0 +1,4 @@
+const video = new (require('./'))(require('got').extend({ mutableDefaults: true }))
+video.cookie = (new (require('@inrixia/db'))('credentials', './credentials.json', false)).cookie
+
+test('subscriptions', () => expect(video.url('dDdmyd8biA')).resolves.toMatch(/https:\/\/Edge\d\d-..\.floatplane\.com\/Videos\/dDdmyd8biA\/360\.mp4/));
