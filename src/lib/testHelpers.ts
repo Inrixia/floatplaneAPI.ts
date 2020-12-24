d86bb12aa3f5ea48016c820b8e7d3bd6ff153189
Inrix <Inrixia@gmail.com>
2020-12-24 15:50:49 +1300

    Updated types& modules

--- src/lib/testHelpers.ts
+++ src/lib/testHelpers.ts
@@ -1,8 +1,8 @@
 import { FileCookieStore } from "tough-cookie-file-store";
-import { CookieJar, Store } from "tough-cookie";
+import { CookieJar } from "tough-cookie";
 
 // TODO: Remove type fix once types are updated for "tough-cookie-file-store"
-export const prepCookieJar = async (): Promise<CookieJar> =>  new CookieJar(new FileCookieStore("./cookieStore.json") as unknown as Store);
+export const prepCookieJar = async (): Promise<CookieJar> =>  new CookieJar(new FileCookieStore("./cookieStore.json"));
 
 import type { Image, ChildImage } from "../types";
 export const imageFormat: Image = {
