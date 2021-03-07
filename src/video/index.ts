d54b396db94300e4f14dcb34260f5b520218ad41
Inrix <Inrixia@gmail.com>
2021-03-07 22:16:00 +1300

    Allow passing undefined for gotOptions

--- src/video/index.ts
+++ src/video/index.ts
@@ -28,5 +28,5 @@ export default class Video extends Core {
 	 * const stream = await download("jImMbJ2pbE", "360")
 	 * stream.pipe(fs.createWriteStream('video.mp4')) // Saves video to 'video.mp4'
 	 */
-	download = async (videoGUID: string, videoQuality="360", gotOptions: GotOptions = { isStream: true }): Promise<Request> => this.got.stream(await this.url(videoGUID, videoQuality), gotOptions)
+	download = async (videoGUID: string, videoQuality="360", gotOptions: GotOptions | undefined = { isStream: true }): Promise<Request> => this.got.stream(await this.url(videoGUID, videoQuality), gotOptions)
 }
