532ee7ad5671da13d4fa5de6e831ee36c378f3a9
Inrix <Inrixia@gmail.com>
2022-09-23 16:57:30 +1200

    Changed require to import for version

--- src/index.ts
+++ src/index.ts
@@ -17,8 +17,9 @@ export type LoginOptions = {
 	token?: string;
 };
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const { version } = require("../package.json");
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore Yes, package.json isnt under src, this is fine
+import { version } from "../package.json";
 
 export const headers = {
 	"User-Agent": `FloatplaneAPI/${version} (Inrix, +https://github.com/Inrixia/floatplaneAPI.ts), CFNetwork`,
