e37b4a0fbd9d2222e6c97fe0799fa8c80b1c5a07
Inrixia <Inrixia@gmail.com>
2023-03-25 23:48:46 +1300

    Added v3 cdn delivery endpoint

--- src/cdn.test.ts
+++ src/cdn.test.ts
@@ -1,7 +1,7 @@
 import { test } from "vitest";
 
 import got from "got";
-import { CDN, VodLiveDeliveryResponse, DownloadDeliveryResponse } from "./cdn.js";
+import { CDN, V2VodLiveDeliveryResponse, V2DownloadDeliveryResponse, DeliveryResponse } from "./cdn.js";
 import { clientFormat, edgeFormat, gotExtends, expect } from "./lib/testHelpers.js";
 
 import type { components } from "./lib/apiSchema.js";
@@ -16,7 +16,7 @@ const qualityLevelFormat: components["schemas"]["CdnDeliveryV2QualityLevelModel"
 	codecs: expect.typeOrNull(String),
 };
 const Strategy = ["cdn", "client"] as const;
-const liveDeliveryResponseFormat: VodLiveDeliveryResponse = {
+const V2liveDeliveryResponseFormat: V2VodLiveDeliveryResponse = {
 	cdn: expect.any(String),
 	strategy: expect.enum(Strategy),
 	resource: {
@@ -27,7 +27,7 @@ const liveDeliveryResponseFormat: VodLiveDeliveryResponse = {
 	},
 };
 
-const downloadDeliveryResponseFormat: DownloadDeliveryResponse = {
+const V2downloadDeliveryResponseFormat: V2DownloadDeliveryResponse = {
 	edges: expect.arrayContainingOrEmpty([edgeFormat]),
 	client: clientFormat,
 	strategy: expect.enum(Strategy),
@@ -40,12 +40,90 @@ const downloadDeliveryResponseFormat: DownloadDeliveryResponse = {
 	},
 };
 
+const origin: components["schemas"]["CdnDeliveryV3Origin"] = {
+	url: expect.any(String),
+	queryUrl: expect.typeOrUndefined(String),
+	datacenter: {
+		latitude: expect.any(Number),
+		longitude: expect.any(Number),
+		countryCode: expect.any(String),
+		regionCode: expect.any(String),
+	},
+};
+const cdnDeliveryV3MediaIdentityCharacteristics: components["schemas"]["CdnDeliveryV3MediaIdentityCharacteristics"] = {
+	codec: expect.typeOrUndefined(String),
+	codecSimple: expect.typeOrUndefined(String),
+	mimeType: expect.typeOrUndefined(String),
+};
+const cdnDeliveryV3ImagePresentationCharacteristics: components["schemas"]["CdnDeliveryV3ImagePresentationCharacteristics"] = {
+	width: expect.typeOrUndefined(Number),
+	height: expect.typeOrUndefined(Number),
+	isHdr: expect.typeOrUndefined(Boolean),
+};
+const cdnDeliveryV3MediaBitrateInfo: components["schemas"]["CdnDeliveryV3MediaBitrateInfo"] = {
+	bitrate: expect.objectContainingOrUndefined({
+		maximum: expect.typeOrUndefined(Number),
+		average: expect.typeOrUndefined(Number),
+	}),
+};
+const cdnDeliveryMeta: components["schemas"]["CdnDeliveryV3Meta"] = {
+	common: {
+		size: expect.typeOrUndefined(Number),
+		access: expect.objectContainingOrUndefined({
+			deniedReason: expect.typeOrUndefined(expect.stringMatching(/isMissingPermission|isProcessing|isBroken/)),
+			deniedMessage: expect.typeOrUndefined(String),
+		}),
+	},
+	video: expect.objectContainingOrUndefined({
+		...cdnDeliveryV3MediaIdentityCharacteristics,
+		...cdnDeliveryV3ImagePresentationCharacteristics,
+		fps: expect.typeOrUndefined(Number),
+		...cdnDeliveryV3MediaBitrateInfo,
+	}),
+	audio: expect.objectContainingOrUndefined({
+		...cdnDeliveryV3MediaIdentityCharacteristics,
+		channelCount: expect.typeOrUndefined(Number),
+		samplerate: expect.typeOrUndefined(Number),
+		...cdnDeliveryV3MediaBitrateInfo,
+	}),
+	image: expect.objectContainingOrUndefined({
+		...cdnDeliveryV3MediaIdentityCharacteristics,
+		...cdnDeliveryV3ImagePresentationCharacteristics,
+		live: expect.objectContainingOrUndefined({
+			lowLatencyExtension: expect.typeOrUndefined(expect.stringMatching(/"llhls"|"clhls"|"ivshls"|"lldash"/)),
+		}),
+	}),
+};
+const variant: components["schemas"]["CdnDeliveryV3Variant"] = {
+	name: expect.any(String),
+	label: expect.any(String),
+	url: expect.any(String),
+	origins: expect.arrayContainingOrEmptyOrUndefined([origin]),
+	order: expect.any(Number),
+	enabled: expect.any(Boolean),
+	hidden: expect.any(Boolean),
+	meta: expect.objectContainingOrUndefined(cdnDeliveryMeta),
+	mimeType: expect.any(String),
+};
+const deliveryResponseFormat: DeliveryResponse = {
+	groups: expect.arrayContaining([
+		expect.objectContaining({
+			origins: expect.arrayContainingOrEmpty([origin]),
+			variants: expect.arrayContaining([variant]),
+		}),
+	]),
+};
+
 const cdn = new CDN(got.extend(gotExtends()));
 
-test('CDN.delivery("live", creator)', () => {
-	return expect(cdn.delivery("live", "59f94c0bdd241b70349eb72b")).resolves.toStrictEqual(liveDeliveryResponseFormat);
+test('CDN.deliveryV2("live", creator)', () => {
+	return expect(cdn.deliveryV2("live", "59f94c0bdd241b70349eb72b")).resolves.toStrictEqual(V2liveDeliveryResponseFormat);
+});
+
+test('CDN.deliveryV2("download", guid)', async () => {
+	return expect(cdn.deliveryV2("download", "InwhyES1dt")).resolves.toStrictEqual(V2downloadDeliveryResponseFormat);
 });
 
 test('CDN.delivery("download", guid)', async () => {
-	return expect(cdn.delivery("download", "InwhyES1dt")).resolves.toStrictEqual(downloadDeliveryResponseFormat);
+	return expect(cdn.delivery("download", "O54NC8QsAy")).resolves.toStrictEqual(deliveryResponseFormat);
 });
--- src/cdn.ts
+++ src/cdn.ts
@@ -7,21 +7,26 @@ import { fillTemplate } from "@inrixia/helpers/object";
 
 export type GotOptions = Options & { isStream: true };
 
-export type VodLiveDeliveryResponse = components["schemas"]["CdnDeliveryV2VodLivestreamResponse"];
-export type DownloadDeliveryResponse = components["schemas"]["CdnDeliveryV2DownloadResponse"];
+// V2
+export type V2VodLiveDeliveryResponse = components["schemas"]["CdnDeliveryV2VodLivestreamResponse"];
+export type V2DownloadDeliveryResponse = components["schemas"]["CdnDeliveryV2DownloadResponse"];
+export type V2DeliveryResponse = components["schemas"]["CdnDeliveryV2Response"];
+type QueryParamsV2 = operations["getDeliveryInfo"]["parameters"]["query"];
 
-export type DeliveryResponse = components["schemas"]["CdnDeliveryV2Response"];
+// V3
+export type DeliveryResponse = components["schemas"]["CdnDeliveryV3Response"];
+type QueryParams = operations["getDeliveryInfoV3"]["parameters"]["query"];
 
-type QueryParams = operations["getDeliveryInfo"]["parameters"]["query"];
 export class CDN extends Core {
 	/**
-	 * Fetches resource information from cdn.
+	 * Fetches resource information from v2 cdn.
+	 * @deprecated This function is deprecated and will be removed in future versions. Use the `delivery` function instead. This only exists for backwards compatibility.
 	 */
-	delivery(type: "live", creator: string): Promise<VodLiveDeliveryResponse>;
-	delivery(type: "download", guid: string): Promise<DownloadDeliveryResponse>;
-	delivery(type: "vod", guid: string): Promise<VodLiveDeliveryResponse>;
-	delivery(type: "aod", guid: string): Promise<VodLiveDeliveryResponse>;
-	delivery(type: QueryParams["type"], id: string): Promise<DeliveryResponse> {
+	deliveryV2(type: "live", creator: string): Promise<V2VodLiveDeliveryResponse>;
+	deliveryV2(type: "download", guid: string): Promise<V2DownloadDeliveryResponse>;
+	deliveryV2(type: "vod", guid: string): Promise<V2VodLiveDeliveryResponse>;
+	deliveryV2(type: "aod", guid: string): Promise<V2VodLiveDeliveryResponse>;
+	deliveryV2(type: QueryParamsV2["type"], id: string): Promise<V2DeliveryResponse> {
 		const url = new URL(this.BaseUrl + ApiPaths.getDeliveryInfo);
 
 		url.searchParams.set("type", type);
@@ -32,10 +37,31 @@ export class CDN extends Core {
 		return this.got(url.href).json();
 	}
 
-	fillUrl(response: DeliveryResponse, quality?: string): string {
+	fillUrlV2(response: V2DeliveryResponse, quality?: string): string {
 		const { data, uri } = response.resource;
 		if (quality === undefined) return fillTemplate(data, uri);
 		if (data.qualityLevels?.some((level) => quality === level.name) !== true) throw new Error(`Quality "${quality}" is not present in response qualityLevels!`);
 		return fillTemplate(data, uri.replaceAll("qualityLevelParams", `qualityLevelParams.${quality}`).replaceAll("{qualityLevels}", quality));
+	}
+
+	/**
+	 * Fetches resource information from cdn.
+	 *
+	 * @param scenario The scenario in which to consume the media.
+	 * - `onDemand` = stream a Video/Audio On Demand
+	 * - `download` = Download the content for the user to play later.
+	 * - `live` = Livestream the content
+	 * @param entityId The attachment or livestream identifier for the requested media. For video and audio, this would be from the `videoAttachments` or `audioAttachments` objects. For livestreams, this is the `liveStream.id` from the creator object.
+	 * @param outputKind Use `outputKind` to ensure the right vehicle is used for your client, e.g. `outputKind=hls.fmp4` is optimal for tvOS 10+.
+	 * @returns A promise that resolves to a `DeliveryResponse` object.
+	 */
+	delivery(scenario: QueryParams["scenario"], entityId: string, outputKind?: QueryParams["outputKind"]): Promise<DeliveryResponse> {
+		const url = new URL(this.BaseUrl + ApiPaths.getDeliveryInfoV3);
+
+		url.searchParams.set("scenario", scenario);
+		url.searchParams.set("entityId", entityId);
+		if (outputKind !== undefined) url.searchParams.set("outputKind", outputKind);
+
+		return this.got(url.href).json();
 	}
 }
