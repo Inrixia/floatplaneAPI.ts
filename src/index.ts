17aec188e7d1d825cc5134d3e9f5f7950e44b540
Inrixia <Inrixia@gmail.com>
2023-04-26 18:29:14 +1200

    4.5.0

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,7 @@ export type LoginOptions = {
 	token?: string;
 };
 
-export const version = "4.4.6";
+export const version = "4.5.0";
 
 export const headers = {
 	"User-Agent": `FloatplaneAPI/${version} (Inrix, +https://github.com/Inrixia/floatplaneAPI.ts), CFNetwork`,
