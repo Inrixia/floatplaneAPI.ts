cf4dec763661cbcaf62150ecba4f13ffb54a1c64
Inrixia <Inrixia@gmail.com>
2023-04-26 18:27:46 +1200

    Added video attachment info endpoint to content

--- src/content.ts
+++ src/content.ts
@@ -5,9 +5,17 @@ import { ApiPaths, type operations } from "./lib/apiSchema.js";
 export type ContentPost = operations["getBlogPost"]["responses"][200]["content"]["application/json"];
 type PostParams = operations["getBlogPost"]["parameters"]["query"];
 
+export type VideoContent = operations["getVideoContent"]["responses"][200]["content"]["application/json"];
+type VideoParams = operations["getVideoContent"]["parameters"]["query"];
+
 export class Content extends Core {
 	/**
 	 * A specific post from the floatplane api by id
 	 */
 	post = (id: PostParams["id"]): Promise<ContentPost> => this.got(`${this.BaseUrl}${ApiPaths.getBlogPost}/?id=${id}`).json();
+
+	/**
+	 * A specific video attachment from the floatplane api by id
+	 */
+	video = (id: VideoParams["id"]): Promise<VideoContent> => this.got(`${this.BaseUrl}${ApiPaths.getVideoContent}/?id=${id}`).json();
 }
