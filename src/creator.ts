ea22455f3344a7892fc19ed5be3710875e5ddfc6
pagdot <paul70079@gmail.com>
2023-09-22 12:09:50 +0200

    Fix missing parameter

--- src/creator.ts
+++ src/creator.ts
@@ -32,7 +32,7 @@ export class Creator extends Core {
 		const url = new URL(this.BaseUrl + ApiPaths.getCreatorBlogPosts);
 		url.searchParams.append("id", creatorGUID);
 		if (options !== undefined) {
-			const { limit, fetchAfter, search, tags, hasVideo, hasAudio, hasPicture, hasText, sort, fromDuration, toDuration, fromDate, toDate } = options;
+			const { limit, fetchAfter, search, tags, hasVideo, hasAudio, hasPicture, hasText, sort, fromDuration, toDuration, fromDate, toDate, channel } = options;
 			if (channel !== undefined) url.searchParams.append("channel", channel.toString());
 			if (limit !== undefined) url.searchParams.append("limit", limit.toString());
 			if (fetchAfter !== undefined) url.searchParams.append("fetchAfter", fetchAfter.toString());
