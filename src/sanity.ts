6675a48097bffb241512cc2868f74db47572bb07
Inrixia <Inrixia@gmail.com>
2025-12-02 17:33:56 +1300

    Update sanity checks

--- src/sanity.ts
+++ src/sanity.ts
@@ -1,7 +1,8 @@
 import { Floatplane, version } from "./index.js";
 const fApi = new Floatplane({ tokenSet: null, clientSettings: { server: "https://auth.floatplane.com/realms/floatplane-pp", clientId: "floatplane-downloader"} }, "test", "https://pp.floatplane.com"); // Create a new API instance.
 
-console.log(await fApi.deviceLogin());
+// console.log(await fApi.deviceLogin());
+
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore Yes, package.json isnt under src, this is fine
 import pkg from "../package.json" with { type: "json" };
